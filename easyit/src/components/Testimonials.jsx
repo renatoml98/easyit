35a6b83daac7b49de7d1ea7d3a11afa70409d3dd
dre-franco <andreffranco98@gmail.com>
2022-09-28 16:11:48 +0100

    changes on Testimonials

--- easyit/src/components/Testimonials.jsx
+++ easyit/src/components/Testimonials.jsx
@@ -9,12 +9,13 @@ const Testimonials = () => (
 
     <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
       <h2 className={styles.heading2}>
-        What People are <br className="sm:block hidden" /> saying about us
+        About our team.
       </h2>
       <div className="w-full md:mt-0 mt-6">
         <p className={`${styles.paragraph} text-left max-w-[450px]`}>
-          Everything you need to accept card payments and grow your business
-          anywhere on the planet.
+        At EasyIT we help develop innovative solutions to simplify operations that would otherwise not be considered the most efficient, we build integrated solutions that revolutionize the way processes are managed.
+        <br className="sm:block hidden" />
+        We are a team of software engineers always focused on surpassing everything we do.
         </p>
       </div>
     </div>
--- easyit/src/constants/index.js
+++ easyit/src/constants/index.js
@@ -1,4 +1,4 @@
-import { people01, people02, people03, facebook, instagram, linkedin, twitter, airbnb, binance, coinbase, dropbox, send, shield, star  } from "../assets";
+import { andre, br, renato, people02, people03, facebook, instagram, linkedin, twitter, airbnb, binance, coinbase, dropbox, send, shield, star  } from "../assets";
 
 export const navLinks = [
   {
@@ -47,26 +47,26 @@ export const feedback = [
   {
     id: "feedback-1",
     content:
-      "Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver.",
+      "Without my cookies... im just a monster. - André Franco, RN (Real Nigga)",
     name: "Herman Jensen",
     title: "Founder & Leader",
-    img: people01,
+    img: andre,
   },
   {
     id: "feedback-2",
     content:
-      "Money makes your life easier. If you're lucky to have it, you're lucky.",
+      "You´re not as bad as you think, you are worse. - Leonado Marrão (Profeta)",
     name: "Steve Mark",
-    title: "Founder & Leader",
-    img: people02,
+    title: "Akatsuki member",
+    img: br,
   },
   {
     id: "feedback-3",
     content:
-      "It is usually people in the money business, finance, and international trade that are really rich.",
+      "People call me trash, why shouldn't i act like it? - Renato Lima (Professional Bald Man)",
     name: "Kenn Gallagher",
     title: "Founder & Leader",
-    img: people03,
+    img: renato,
   },
 ];
 
