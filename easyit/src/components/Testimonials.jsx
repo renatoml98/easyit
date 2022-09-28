13d0f41277b6bf2fffaf88875f449e5f0606db32
dre-franco <andreffranco98@gmail.com>
2022-09-28 16:40:11 +0100

    update

--- easyit/src/components/Testimonials.jsx
+++ easyit/src/components/Testimonials.jsx
@@ -8,12 +8,12 @@ const Testimonials = () => (
     <div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient bottom-40" />
 
     <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
-      <h2 className={styles.heading2}>
+      <h2 className={`${styles.heading2} text-left max-w-[40%]`}>
         About our team.
       </h2>
       <div className="w-full md:mt-0 mt-6">
-        <p className={`${styles.paragraph} text-left max-w-[450px]`}>
-        At EasyIT we help develop innovative solutions to simplify operations that would otherwise not be considered the most efficient, we build integrated solutions that revolutionize the way processes are managed.
+        <p className={`${styles.paragraph} text-left max-w-[80%]`}>
+        At EasyIT we help develop innovative solutions to simplify operations that wouldn´t otherwise be considered efficient, we build integrated solutions that revolutionize the way processes are managed.
         <br className="sm:block hidden" />
         We are a team of software engineers always focused on surpassing everything we do.
         </p>
